31f46131fc0b2bf53af1eff84a7c7d89ee221b74
adish17 <adishirfan17@gmail.com>
2024-09-26 12:36:06 +0530

    range of Byte Short Long Int

---
+++ ByteShortIntLong/src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var age = 22
+
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println("Int maximum value is: ${maxIntegerValue}")
+    println("Int maximum value is: ${minIntegerValue}")
+
+    val maxByteValue: Byte = Byte.MAX_VALUE
+    val minByteValue: Byte = Byte.MIN_VALUE
+
+    println("Byte maximum value is: ${maxByteValue}")
+    println("Byte minimum value is: ${minByteValue}")
+
+    val maxShortValue: Short = Short.MAX_VALUE
+    val minShortValue: Short = Short.MIN_VALUE
+
+    println("Byte maximum value is: ${maxShortValue}")
+    println("Byte minimum value is: ${minShortValue}")
+
+    val myNumber = 7464654212413521
+
+}
