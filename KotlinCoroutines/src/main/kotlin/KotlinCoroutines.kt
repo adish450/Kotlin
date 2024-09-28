04bef69cff85e330bbaaa8e1fbb01fae82bc98d4
adish17 <adishirfan17@gmail.com>
2024-09-28 21:19:28 +0530

    job.join()

--- KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
+++ KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
@@ -5,15 +5,13 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     println("Main program starts: ${Thread.currentThread().name}")
-    GlobalScope.launch {// Thread: T1
+    val job = launch {// Thread: T1
         println("Fake work starts: ${Thread.currentThread().name}")
         delay(1000) // Coroutine is suspended but thread T1 is free (not blocked)
         println("Fake work ends:  ${Thread.currentThread().name}") //Either T1 or some other thread
     }
-    suspendThread(2000)
-    println("Main program ends: ${Thread.currentThread().name}")
-}
 
-suspend fun suspendThread(time: Long) {
-    delay(time)
+    job.join() // waits till the coroutine is complete
+
+    println("Main program ends: ${Thread.currentThread().name}")
 }
