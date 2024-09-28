08a2d2a74a26e1ce7eb4685cac4e58e9686712ae
adish17 <adishirfan17@gmail.com>
2024-09-28 19:38:06 +0530

    optimised way to use runBlocking

--- KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
+++ KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
@@ -2,19 +2,14 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.concurrent.thread
 
-fun main() {
+fun main() = runBlocking {
     println("Main program starts: ${Thread.currentThread().name}")
-
     GlobalScope.launch {// Thread: T1
         println("Fake work starts: ${Thread.currentThread().name}")
         delay(1000) // Coroutine is suspended but thread T1 is free (not blocked)
         println("Fake work ends:  ${Thread.currentThread().name}") //Either T1 or some other thread
     }
-    runBlocking { // creates a coroutine which block the current thread (main thread)
-        delay(2000)
-    }
-
+    delay(2000)
     println("Main program ends: ${Thread.currentThread().name}")
 }
