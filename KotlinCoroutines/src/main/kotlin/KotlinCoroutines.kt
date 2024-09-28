8e1bedf2040fcb6795e4241d35b6c21a1d697fa6
adish17 <adishirfan17@gmail.com>
2024-09-28 22:51:31 +0530

    cooperative using delay()

--- KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
+++ KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
@@ -2,15 +2,16 @@ import kotlinx.coroutines.*
 
 fun main() = runBlocking {
     println("Main program starts: ${Thread.currentThread().name}")
-    val jobDeferred = async {// Thread: T1
-        println("Fake work starts: ${Thread.currentThread().name}")
-        delay(1000) // Coroutine is suspended but thread T1 is free (not blocked)
-        println("Fake work ends:  ${Thread.currentThread().name}") //Either T1 or some other thread
-        15
+
+    val job = launch() {
+        for (i in 0..500) {
+            print("$i ")
+            delay(1)
+        }
     }
 
-    //jobDeferred.join() // waits till the coroutine is complete
-    println(jobDeferred.await())
+    delay(10)
+    job.cancelAndJoin()
 
     println("Main program ends: ${Thread.currentThread().name}")
 }
