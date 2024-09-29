7c5bde9d26a770aef05ecb2c9ec532b0c4024cd0
adish17 <adishirfan17@gmail.com>
2024-09-29 16:01:27 +0530

    withTimeOut() coroutine builder

--- KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
+++ KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
@@ -3,25 +3,19 @@ import kotlinx.coroutines.*
 fun main() = runBlocking {
     println("Main program starts: ${Thread.currentThread().name}")
 
-    val job = launch {
-        try {
+    try {
+        withTimeout(2000) {
             for (i in 0..500) {
                 print("$i ")
-                delay(1000)
-            }
-        } catch (ex: CancellationException) {
-            println("\nException caught safely : ${ex.message}")
-        } finally {
-            withContext(NonCancellable) {//coroutine builder
-                delay(1000) //Generally we don't use this
-                println("\nClose resources in finally")
+                delay(500)
             }
         }
+    } catch (ex: TimeoutCancellationException) {
+        println(ex.message)
+    } finally {
+
     }
 
-    delay(10)
-    job.cancel(CancellationException("My custom message"))
-    job.join()
 
     println("Main program ends: ${Thread.currentThread().name}")
 }
