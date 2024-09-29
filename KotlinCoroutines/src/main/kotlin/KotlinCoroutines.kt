44ec36d1d60c205fcebfec8a63389184d7dc7db5
adish17 <adishirfan17@gmail.com>
2024-09-29 20:01:15 +0530

    Coroutines Dispatchers

--- KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
+++ KotlinCoroutines/src/main/kotlin/KotlinCoroutines.kt
@@ -4,23 +4,31 @@ import kotlin.time.measureTime
 fun main() = runBlocking {
     println("Main program starts: ${Thread.currentThread().name}")
 
-    val time = measureTime {
-        val msgOne = async (start = CoroutineStart.LAZY) { msgOne() }
-        val msgTwo = async (start = CoroutineStart.LAZY) { msgTwo() }
-        //println("${msgOne.await()} ${msgTwo.await()}")
+    println("CoroutineScope: $this")
+
+    launch { //No dispatcher passed, inherits the context of the parent coroutine (runBlocking)
+        println("C1: ${Thread.currentThread().name}") //Thread: main
+        delay(1000)
+        println("C1 after delay: ${Thread.currentThread().name}") //Thread: main
     }
 
-    println(time)
+    launch(Dispatchers.Default) { //similar to GlobalScope.launch coroutine
+        println("C2: ${Thread.currentThread().name}") //Thread: T1
+        delay(1000)
+        println("C2 after delay: ${Thread.currentThread().name}") //Thread: T1 or some other thread
+
+        launch(coroutineContext) { //Inherits the parent coroutine (C2) behaves like confined
+            println("C4: ${Thread.currentThread().name}") //Thread: T1
+            delay(1000)
+            println("C4 after delay: ${Thread.currentThread().name}") //Thread: T1
+        }
+    }
+
+    launch(Dispatchers.Unconfined) { //Inherits the parent coroutine (runBlocking)
+        println("C3: ${Thread.currentThread().name}") //Thread: main
+        delay(1000)
+        println("C3 after delay: ${Thread.currentThread().name}") //Thread: some other thread
+    }
 
     println("Main program ends: ${Thread.currentThread().name}")
-}
-
-suspend fun msgOne(): String {
-    delay(1000L)
-    return "Hello!, "
-}
-
-suspend fun msgTwo(): String {
-    delay(1000L)
-    return "World"
 }
